1a70fd2e3887234b8ed6e637244d0d41a604ac99
Spunksai@09 <b.jayaditya@gmail.com>
2021-05-30 15:55:42 +0530

    clone modified

--- src/Version_1/Git_Ver_01.java
+++ src/Version_1/Git_Ver_01.java
@@ -3,7 +3,7 @@ package Version_1;
 public class Git_Ver_01 {
 
 	public static void main(String[] args) {
-		System.out.println("Git Default Method");
+		System.out.println("Git updated method");
 
 	}
 
